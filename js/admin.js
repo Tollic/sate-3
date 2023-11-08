72b5e09c66abf00532d41c9f68ef8b145912d17b
Tollic <72671011+Tollic@users.noreply.github.com>
2023-11-08 11:40:37 +0200

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,7 @@
 function createBlocks() {
     var word = document.getElementById('wordInput').value;
     localStorage.setItem('word', word);
-    var newTab = window.open('index.html', '_blank');
+    var newTab = window.open('index2.html', '_blank');
 }
 
 function creatBlocks() {
